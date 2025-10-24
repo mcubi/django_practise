c5ab369cebe8fb7706c8bdba76233d7792d5cc7d
Azel <marcos.mose@colegiomiralmonte.es>
2025-10-24 20:36:33 +0200

    Few changes of names to have more coherence

--- myshop/urls.py
+++ myshop/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path("__reload__", include("django_browser_reload.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
